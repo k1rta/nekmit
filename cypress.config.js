b85a5611b73a961b020e4626a44bd2e893181e8f
Kirta-Linda Karits <kirta@Kirta-Lindas-MacBook-Air.local>
2023-05-31 15:36:20 +0300

    chanes in text and tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  numTestsKeptInMemory: 15,
+  defaultCommandTimeout: 15000,
   e2e: {
     setupNodeEvents(on, config) {
     },
