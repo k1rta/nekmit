8a11c6624468568f93eba34aa8dffd0675a6bb41
k1rta <31627919+k1rta@users.noreply.github.com>
2025-02-17 15:01:26 +0200

    Prod localhost fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from "public" directory with correct URL structure
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+// Serve static files (CSS, JS, Images) directly from root-level "assets" folder
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // Serve index.html from the root directory
 app.get('/', (req, res) => {
