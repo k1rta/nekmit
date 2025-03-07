64cc88848be86bd503ffbc5dad3dc4244a41b3f6
k1rta <31627919+k1rta@users.noreply.github.com>
2025-03-07 16:57:00 +0200

    fix: improvements

--- cypress/e2e/page_view_api_success.cy.js
+++ cypress/e2e/page_view_api_success.cy.js
@@ -1,12 +1,12 @@
-describe('Pageview API - Success Cases', () => {
+describe('Page View API - Success Cases', () => {
   beforeEach(() => {
-    cy.intercept('POST', '/api/pageview').as('pageview')
+    cy.intercept('POST', '/api/page_view').as('page_view')
     cy.visit('')
   })
 
   it('should validate API response schema', () => {
-    cy.wait('@pageview').then((interception) => {
-      cy.validateApiSchema(interception.response.body, 'pageview.json')
+    cy.wait('@page_view').then((interception) => {
+      cy.validateApiSchema(interception.response.body, 'page_view.json')
 
       const expectedPage = Cypress.config('baseUrl').endsWith('nekmit/') ? '/nekmit/' : '/'
       expect(interception.response.body).to.have.property('page', expectedPage)
@@ -18,26 +18,26 @@ describe('Pageview API - Success Cases', () => {
     })
   })
 
-  it('should send the correct request body when recording a pageview', () => {
-    cy.wait('@pageview').then((interception) => {
+  it('should send the correct request body when recording a pageView', () => {
+    cy.wait('@page_view').then((interception) => {
       expect(interception.request.headers).to.have.property('content-type', 'application/json')
       cy.validateRequestBody(interception.request.body)
     })
   })
 
-  it('should return HTTP 200 when pageview is recorded successfully', () => {
-    cy.wait('@pageview').then((interception) => {
+  it('should return HTTP 200 when page view is recorded successfully', () => {
+    cy.wait('@page_view').then((interception) => {
       expect(interception.response.statusCode).to.eq(200)
       expect(interception.response.body).to.have.property(
         'message',
-        'Pageview recorded in Firebase'
+        'Page view recorded in Firebase'
       )
       expect(interception.response.body).to.have.property('timestamp').that.is.a('string')
     })
   })
 
-  it('should ensure the pageview API is called only once per page load', () => {
+  it('should ensure the page view API is called only once per page load', () => {
     cy.reload()
-    cy.wait('@pageview').its('response.statusCode').should('eq', 200)
+    cy.wait('@page_view').its('response.statusCode').should('eq', 200)
   })
 })
